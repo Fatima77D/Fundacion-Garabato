c3d30bfa322905a21509a650b5ba5b6d4772f943
“Gianna <giannagiavarini@outlook.com>
2024-02-03 10:38:15 -0300

    se modifica seccion sumate y donar otro monto

---
+++ js/donar_otro_monto.js
@@ -0,0 +1,7 @@
+let parrafoCredicoop = document.getElementById("pFigureCredicoop");
+
+
+if (window.innerWidth <= 768) {
+
+    parrafoCredicoop.textContent = "Haciendo click en el botón de credicoop podes donar el monto que vos desees 🙌";
+}
